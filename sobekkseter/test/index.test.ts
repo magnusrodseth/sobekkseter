8ca2a1fb1b25987c1ffd7d58329732e495e51c93
Magnus Rødseth <magnus.rodseth@gmail.com>
2021-06-05 15:25:31 +0200

    Add sample test

--- sobekkseter/test/index.test.ts
+++ sobekkseter/test/index.test.ts
@@ -3,5 +3,7 @@ import conditions from "../pages/api/conditions";
 describe("My first test 🏁", () => {
     it("should do nothing", () => {
         expect(3).toBe(3);
+        // This is a test change that should pass all new tests
+        expect(5).toBe(5);
     })
 })
