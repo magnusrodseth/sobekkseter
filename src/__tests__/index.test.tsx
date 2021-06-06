98606c3ba703428ec34a5d09655ef945ef5735fe
Magnus Rødseth <magnus.rodseth@gmail.com>
2021-06-06 19:41:29 +0200

    Add proper API call

--- src/__tests__/index.test.tsx
+++
@@ -1,9 +0,0 @@
-import conditions from "../pages/api/conditions";
-
-describe("My first test 🏁", () => {
-    it("should do nothing", () => {
-        expect(3).toBe(3);
-        // This is a test change that should pass all new tests
-        expect(5).toBe(5);
-    })
-})
