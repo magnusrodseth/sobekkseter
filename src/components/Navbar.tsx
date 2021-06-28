62ebeae1553f30009e8b9ce13e325e6ece801a25
Magnus Rødseth <magnus.rodseth@gmail.com>
2021-06-28 09:30:24 +0200

    add modal to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,13 @@
-import React from "react";
-import { Disclosure, Menu } from "@headlessui/react";
+import React, { Fragment } from "react";
+import { Dialog, Disclosure, Menu, Transition } from "@headlessui/react";
 import classNames from "../utils/classNames";
-import MenuIcon from "./icons/MenuIcon";
-import XIcon from "./icons/XIcon";
+import {
+  MenuIcon,
+  XIcon,
+  QuestionMarkCircleIcon,
+} from "@heroicons/react/outline";
+import { useState } from "react";
+import { useRef } from "react";
 
 const navigation = [
   {
@@ -25,6 +30,14 @@ const navigation = [
 ];
 
 const Navbar = () => {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  const cancelButtonRef = useRef(null);
+
+  const handleModalIsOpen = () => {
+    setModalIsOpen(!modalIsOpen);
+  };
+
   return (
     <Disclosure as="nav" className="font-mono text-center">
       {({ open }) => (
@@ -57,7 +70,7 @@ const Navbar = () => {
                   </a>
                 </div>
                 <div className="hidden sm:block sm:ml-6">
-                  <div className="flex space-x-4">
+                  <div className="flex items-center space-x-4">
                     {navigation.map((item) => (
                       <a
                         key={item.name}
@@ -73,6 +86,14 @@ const Navbar = () => {
                         {item.name}
                       </a>
                     ))}
+                    <QuestionMarkCircleIcon
+                      className={classNames(
+                        "w-8 h-8 text-blue-400",
+                        "transition duration-500 ease-in-out transform",
+                        "hover:-translate-y-1 cursor-pointer"
+                      )}
+                      onClick={handleModalIsOpen}
+                    />
                   </div>
                 </div>
               </div>
@@ -80,7 +101,7 @@ const Navbar = () => {
           </div>
 
           <Disclosure.Panel className="sm:hidden">
-            <div className="px-2 pt-2 pb-3 space-y-1">
+            <div className="px-2 pt-2 pb-3 flex flex-col justify-center items-center space-y-1">
               {navigation.map((item) => (
                 <a
                   key={item.name}
@@ -96,8 +117,122 @@ const Navbar = () => {
                   {item.name}
                 </a>
               ))}
+              <QuestionMarkCircleIcon
+                className={classNames(
+                  "w-8 h-8 text-blue-400",
+                  "transition duration-500 ease-in-out transform",
+                  "hover:-translate-y-1 cursor-pointer"
+                )}
+                onClick={handleModalIsOpen}
+              />
             </div>
           </Disclosure.Panel>
+
+          {/* Information modal */}
+          <Transition.Root show={modalIsOpen} as={Fragment}>
+            <Dialog
+              as="div"
+              static
+              className="fixed z-10 inset-0 overflow-y-auto"
+              initialFocus={cancelButtonRef}
+              open={modalIsOpen}
+              onClose={setModalIsOpen}
+            >
+              <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+                <Transition.Child
+                  as={Fragment}
+                  enter="ease-out duration-300"
+                  enterFrom="opacity-0"
+                  enterTo="opacity-100"
+                  leave="ease-in duration-200"
+                  leaveFrom="opacity-100"
+                  leaveTo="opacity-0"
+                >
+                  <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+                </Transition.Child>
+
+                {/* This element is to trick the browser into centering the modal contents. */}
+                <span
+                  className="hidden sm:inline-block sm:align-middle sm:h-screen"
+                  aria-hidden="true"
+                >
+                  &#8203;
+                </span>
+                <Transition.Child
+                  as={Fragment}
+                  enter="ease-out duration-300"
+                  enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                  enterTo="opacity-100 translate-y-0 sm:scale-100"
+                  leave="ease-in duration-200"
+                  leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                  leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                >
+                  <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+                    <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                      <div className="sm:flex sm:items-start">
+                        <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                          <Dialog.Title
+                            as="h2"
+                            className="text-4xl py-6 leading-6 font-medium text-gray-900"
+                          >
+                            Mer informasjon
+                          </Dialog.Title>
+                          <div className="mt-2 p-4 text-left">
+                            <ul className="text-md text-gray-700 list-disc tracking-wide leading-8">
+                              <li className={classNames("my-4")}>
+                                Værdata oppdateres hvert 15. minutt hele døgnet.
+                                Webkamera oppdateres hvert 15. minutt ved
+                                dagslys.
+                              </li>
+                              <li className={classNames("my-4")}>
+                                Værprognosen er kalkulert ut fra forandringen i
+                                lufttrykket pr. time <u>lokalt</u> og gir en
+                                antydning om kommende værforhold de neste 24
+                                timene.
+                              </li>
+                              <li className={classNames("my-4")}>
+                                Duggpunktet gir informasjon om faren for tåke og
+                                kondensering / ising. Når duggpunktet er lik
+                                utetemperaturen, får vi tåke.
+                              </li>
+                              <li className={classNames("my-4")}>
+                                Lufttrykkforandringen er svært viktig for å
+                                bedømme kommende vind. Jo større og raskere
+                                forandring (særlig fallende), desto mer vind har
+                                vi i vente.
+                              </li>
+                              <li className={classNames("my-4")}>
+                                Nedbørsmåler registrerer bare regn (ikke snø).
+                              </li>
+                              <li className={classNames("my-4")}>
+                                Minimums- og Maksimumsresultater nullstilles
+                                hver dag kl. 00:00.
+                              </li>
+                            </ul>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                      <button
+                        type="button"
+                        className={classNames(
+                          "w-full inline-flex justify-center rounded-md border border-transparent",
+                          "shadow-sm px-4 py-2 bg-green-500 text-base font-medium",
+                          "text-white hover:bg-green-700 focus:outline-none focus:ring-2",
+                          "focus:ring-offset-2 focus:ring-green-500 sm:ml-3 sm:w-auto sm:text-sm",
+                          "transition duration-500 ease-in-out transform"
+                        )}
+                        onClick={() => setModalIsOpen(false)}
+                      >
+                        OK
+                      </button>
+                    </div>
+                  </div>
+                </Transition.Child>
+              </div>
+            </Dialog>
+          </Transition.Root>
         </>
       )}
     </Disclosure>
--- src/components/widgets/SimpleWindWidget.tsx
+++ src/components/widgets/SimpleWindWidget.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { DEGREES, METERS_PER_SECOND } from "../../constants";
 import calculateMphToMs from "../../utils/calculateMphToMs";
-import ArrowIcon from "../icons/ArrowIcon";
+import { ArrowUpIcon } from "@heroicons/react/outline";
 import Wrapper from "../Wrapper";
 
 interface SimpleWindWidgetProps {
