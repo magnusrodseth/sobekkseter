db0a0995ba7635803b52fa1a9599542fbb30db48
magnusrodseth <59113973+magnusrodseth@users.noreply.github.com>
2022-12-19 10:01:33 +0100

    fix final lighthouse warnings

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,7 +8,7 @@ const Footer = () => {
     sm:grid-cols-1 sm:grid-rows-2 md:grid-cols-1	md:grid-rows-2 lg:grid-rows-1"
     >
       <Link
-        className="transform text-center text-blue-500 transition duration-500 ease-in-out  hover:text-blue-600 hover:underline"
+        className="transform text-center text-blue-600 transition duration-500 ease-in-out  hover:text-blue-800 hover:underline"
         href="https://github.com/magnusrodseth"
         target="_blank"
         rel="noopener noreferrer"
@@ -17,7 +17,7 @@ const Footer = () => {
       </Link>
 
       <Link
-        className="transform text-center text-blue-500 transition duration-500 ease-in-out  hover:text-blue-600 hover:underline"
+        className="transform text-center text-blue-600 transition duration-500 ease-in-out  hover:text-blue-800 hover:underline"
         href="https://www.weatherlink.com/"
         target="_blank"
         rel="noopener noreferrer"
