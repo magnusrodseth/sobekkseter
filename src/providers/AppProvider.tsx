639bcae62667c5e527ea228584eeba886a396ac1
magnusrodseth <59113973+magnusrodseth@users.noreply.github.com>
2022-12-19 09:35:34 +0100

    add analytics

--- src/providers/AppProvider.tsx
+++ src/providers/AppProvider.tsx
@@ -1,12 +1,19 @@
 import Layout from "@/components/Layout";
 import type { FC } from "react";
+import { Analytics } from "@vercel/analytics/react";
 
 type AppProviderProps = {
   children: React.ReactNode;
 };
 
 const AppProvider: FC<AppProviderProps> = ({ children }) => {
-  return <Layout>{children}</Layout>;
+  return (
+    <Layout>
+      {children}
+
+      <Analytics />
+    </Layout>
+  );
 };
 
 export default AppProvider;
