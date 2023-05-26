59bd3d2ab11561cb994b503c05a23c073bd3bf6a
magnusrodseth <magnus.rodseth@gmail.com>
2023-05-26 14:57:00 +0200

    feat: google ads

--- src/providers/AppProvider.tsx
+++ src/providers/AppProvider.tsx
@@ -1,6 +1,7 @@
 import Layout from "@/components/Layout";
 import type { FC } from "react";
 import { Analytics } from "@vercel/analytics/react";
+import Script from "next/script";
 
 type AppProviderProps = {
   children: React.ReactNode;
@@ -9,10 +10,17 @@ type AppProviderProps = {
 const AppProvider: FC<AppProviderProps> = ({ children }) => {
   return (
     <Layout>
-
       {children}
 
       <Analytics />
+
+      <Script
+        id="adsbygoogle-init"
+        strategy="afterInteractive"
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3731844450515812"
+        crossOrigin="anonymous"
+      ></Script>
     </Layout>
   );
 };
