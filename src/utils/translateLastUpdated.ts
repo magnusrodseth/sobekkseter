d93d72653e3959baf218262cdd2fb04ab8e1d777
magnusrodseth <magnus.rodseth@gmail.com>
2023-06-23 23:42:03 +0200

    better norwegian parsing of date

--- src/utils/translateLastUpdated.ts
+++ src/utils/translateLastUpdated.ts
@@ -1,5 +1,34 @@
 import moment from "moment";
 
+const monthNumberToNorwegianMonthName = (monthNumber: number) => {
+  switch (monthNumber) {
+    case 1:
+      return "januar";
+    case 2:
+      return "februar";
+    case 3:
+      return "mars";
+    case 4:
+      return "april";
+    case 5:
+      return "mai";
+    case 6:
+      return "juni";
+    case 7:
+      return "juli";
+    case 8:
+      return "august";
+    case 9:
+      return "september";
+    case 10:
+      return "oktober";
+    case 11:
+      return "november";
+    case 12:
+      return "desember";
+  }
+};
+
 /**
  * Translates the last updated date to a more readable Norwegian format.
  * The message is on the format `Last Updated on Jun 23 2023, 11:07 pm CEST`.
@@ -15,8 +44,14 @@ const translateLastUpdated = (lastUpdated: string) => {
   const day = date?.split(" ")[1];
   const year = date?.split(" ")[2];
 
-  const parsedDate = moment(`${day} ${month} ${year}, ${time}`).locale("no");
-  const formattedDate = parsedDate.format("DD.MM.YYYY, HH:mm");
+  // We parse each part of the date to the correct format because the `lastUpdated` date is not on a standard format.
+  const parsedTime = moment(time, "hh:mm a").format("HH:mm");
+  const parsedDay = moment(day, "DD").format("DD");
+  const parsedMonth = monthNumberToNorwegianMonthName(
+    parseInt(moment(month, "MMM").format("M"))
+  );
+  const parsedYear = moment(year, "YYYY").format("YYYY");
+  const formattedDate = `${parsedDay}. ${parsedMonth} ${parsedYear}, ${parsedTime}`;
 
   return `Sist oppdatert ${formattedDate}`;
 };
